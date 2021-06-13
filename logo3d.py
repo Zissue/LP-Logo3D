7ccdb60b1e6ee9fcc89f68aa827a7691e9bd5a6b
Zissue <zi-xuan.sun@estudiantat.upc.edu>
2021-06-13 20:53:05 +0200

    logo3d interpreter commented

--- logo3d.py
+++ logo3d.py
@@ -1,3 +1,11 @@
+###################################### 
+###################################### 
+### Author: Zixuan Sun 
+### [LP] 2020-2021 - Q2 - Logo3D 
+### Group: 22 L
+###################################### 
+###################################### 
+
 import sys
 from antlr4 import *
 from logo3dLexer import logo3dLexer
@@ -5,12 +13,9 @@ from logo3dParser import logo3dParser
 
 from visitor import TreeVisitor
 
+#####################################
 
 
-#input_stream = InputStream(input('? '))
-#input_stream = FileStream(sys.argv[1])
-
-#print(sys.argv)
 if len(sys.argv) <= 1:
     sys.exit("\n[USAGE]: Execute with: python3 script.py [file] (first_procedure_name) (parameters)\n")
 
@@ -23,7 +28,8 @@ token_stream = CommonTokenStream(lexer)
 parser = logo3dParser(token_stream)
 tree = parser.root() 
 
-print(tree.toStringTree(recog = parser)) # debugging
+# For debugging, print the parse tree
+#print(tree.toStringTree(recog = parser)) 
 
 # Case: python3 script.py [file] [procedure_name] [parameters]
 if len(sys.argv) == 3:
@@ -33,10 +39,12 @@ if len(sys.argv) == 3:
 elif len(sys.argv) > 3:
     firstProcedure = sys.argv[2]
     listParameters = sys.argv[3:]
-    for i in range(0,len(listParameters)):
+    for i in range(0, len(listParameters)):
         listParameters[i] = float(listParameters[i])
     visitor = TreeVisitor(firstPROC = firstProcedure, paramInvoc = listParameters)
 else:
     visitor = TreeVisitor()
+
+print("\nExecuting '", sys.argv[1], "' Logo3D program...\n\n", sep="")
 
 visitor.visit(tree)
